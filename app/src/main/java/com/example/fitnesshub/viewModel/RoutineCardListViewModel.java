e787b7635f39f1114c402dfcf2e83eb427774220
Francisco <frbernad@itba.edu.ar>
2020-11-09 00:13:53 -0300

    Auto stash before merge of "main" and "origin/main"

---
+++ app/src/main/java/com/example/fitnesshub/viewModel/RoutineCardListViewModel.java
@@ -0,0 +1,62 @@
+package com.example.fitnesshub.viewModel;
+
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.MutableLiveData;
+
+import com.example.fitnesshub.model.RoutineCardInfo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RoutineCardListViewModel extends AndroidViewModel {
+
+    private MutableLiveData<List<RoutineCardInfo>> routineCards = new MutableLiveData<>();
+    private MutableLiveData<Boolean> routineCardLoadError = new MutableLiveData<>();
+    private MutableLiveData<Boolean> loading = new MutableLiveData<>();
+
+    public RoutineCardListViewModel(@NonNull Application application) {
+        super(application);
+    }
+
+    public void refresh() {
+        RoutineCardInfo rci1 = new RoutineCardInfo("Ejer 1","John","",null,10,3);
+        RoutineCardInfo rci2 = new RoutineCardInfo("Ejer 2","Juan","",null,10,2);
+        RoutineCardInfo rci3 = new RoutineCardInfo("Ejer 3","Philipe","",null,10,5);
+        RoutineCardInfo rci4 = new RoutineCardInfo("Ejer 4","Andrea","",null,10,1);
+
+        ArrayList<RoutineCardInfo> routines = new ArrayList<>();
+        routines.add(rci1);
+        routines.add(rci2);
+        routines.add(rci3);
+        routines.add(rci4);routines.add(rci3);
+        routines.add(rci4);routines.add(rci3);
+        routines.add(rci4);routines.add(rci3);
+        routines.add(rci4);routines.add(rci3);
+        routines.add(rci4);routines.add(rci3);
+        routines.add(rci4);routines.add(rci3);
+        routines.add(rci4);routines.add(rci3);
+        routines.add(rci4);routines.add(rci3);
+        routines.add(rci4);routines.add(rci3);
+        routines.add(rci4);routines.add(rci3);
+        routines.add(rci4);
+
+        routineCards.setValue(routines);
+        routineCardLoadError.setValue(false);
+        loading.setValue(false);
+    }
+
+    public MutableLiveData<List<RoutineCardInfo>> getRoutineCards() {
+        return routineCards;
+    }
+
+    public MutableLiveData<Boolean> getRoutineCardLoadError() {
+        return routineCardLoadError;
+    }
+
+    public MutableLiveData<Boolean> getLoading() {
+        return loading;
+    }
+}
