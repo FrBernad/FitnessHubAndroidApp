77e914a01bdb12d0049a3c60a48f757ad83915d1
Francisco <frbernad@itba.edu.ar>
2020-11-09 00:18:19 -0300

    Auto stash before merge of "main" and "origin/main"

--- app/src/main/java/com/example/fitnesshub/model/RoutinesAPI.java
+++ app/src/main/java/com/example/fitnesshub/model/RoutinesAPI.java
@@ -1,5 +1,9 @@
 package com.example.fitnesshub.model;
 
+<<<<<<< Updated upstream
 public interface RoutinesAPI{
 //    Single<List<RoutineOverviewInfo>>
+=======
+public interface RoutinesAPI  {
+>>>>>>> Stashed changes
 }
