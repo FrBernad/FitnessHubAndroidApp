a48613925b83cf1ae53c1146faa8153b688db778
Nicolás Rampoldi <nrampoldi@itba.edu.ar>
2020-11-10 17:03:36 -0300

    Validations for register

--- app/src/main/java/com/example/fitnesshub/view/register.java
+++ app/src/main/java/com/example/fitnesshub/view/register.java
@@ -9,6 +9,7 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.fitnesshub.R;
+import com.google.android.material.textfield.TextInputLayout;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -16,6 +17,9 @@ import com.example.fitnesshub.R;
  * create an instance of this fragment.
  */
 public class register extends Fragment {
+
+    //For validation
+    TextInputLayout email, username, password;
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -55,6 +59,10 @@ public class register extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+        email = getView().findViewById(R.id.register_email);
+        username = getView().findViewById(R.id.register_username);
+        password = getView().findViewById(R.id.register_password);
+
     }
 
     @Override
@@ -63,4 +71,71 @@ public class register extends Fragment {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_register, container, false);
     }
+
+    //Validation functions
+
+    private boolean validateEmail() {
+        String val = email.getEditText().getText().toString().trim();
+        String checkEmail = "[a-zA-Z0-9._-]+@[a-z]+.+[a-z]+";
+
+        if (val.isEmpty()) {
+            email.setError("Field can not be empty");
+            return false;
+        } else if (!val.matches(checkEmail)) {
+            email.setError("Invalid Email!");
+            return false;
+        } else {
+            email.setError(null);
+            email.setErrorEnabled(false);
+            return true;
+        }
+    }
+
+    private boolean validateUsername() {
+        String val = username.getEditText().getText().toString().trim();
+        String checkspaces = "\\A\\w{1,20}\\z"; //Check whitespaces
+
+        if (val.isEmpty()) {
+            username.setError("Field can not be empty");
+            return false;
+        } else if (val.length() > 20) {
+            username.setError("Username is too large!");
+            return false;
+        } else if (!val.matches(checkspaces)) {
+            username.setError("No white spaces are allowed!");
+            return false;
+        } else {
+            username.setError(null);
+            username.setErrorEnabled(false);
+            return true;
+        }
+    }
+
+    private boolean validatePassword() {
+        String val = password.getEditText().getText().toString().trim();
+        String checkPassword = "^" +
+                "(?=.*[a-zA-Z])" +      //any letter
+                "(?=S+$)" +           //no white spaces
+                ".{4,}" +               //at least 4 characters
+                "$";;
+
+        if (val.isEmpty()) {
+            password.setError("Field can not be empty");
+            return false;
+        } else if (!val.matches(checkPassword)) {
+            password.setError("Password should contain at least 4 characters!");
+            return false;
+        } else {
+            password.setError(null);
+            password.setErrorEnabled(false);
+            return true;
+        }
+    }
+
+    //Cuando se llame al metodo NEXT fragment hay que hacer esto
+//    if(!validateEmail() | !validateUsername() | !validatePassword()) {
+//        return;
+//    }
+
+
 }
