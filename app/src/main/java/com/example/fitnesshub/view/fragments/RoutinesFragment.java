85b2817450c4c9266f71806d285ed6ea31e60488
Nicolás Rampoldi <nrampoldi@itba.edu.ar>
2020-11-11 01:15:34 -0300

    Started working on sort and filter front end

--- app/src/main/java/com/example/fitnesshub/view/fragments/RoutinesFragment.java
+++ app/src/main/java/com/example/fitnesshub/view/fragments/RoutinesFragment.java
@@ -1,11 +1,13 @@
 package com.example.fitnesshub.view.fragments;
 
+import android.app.Dialog;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.core.widget.NestedScrollView;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -13,8 +15,13 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ProgressBar;
+import android.widget.Spinner;
+import android.widget.TextView;
 
+import com.example.fitnesshub.R;
 import com.example.fitnesshub.databinding.FragmentRoutinesBinding;
 import com.example.fitnesshub.view.adapters.RoutinesAdapter;
 import com.example.fitnesshub.viewModel.RoutineListViewModel;
@@ -23,9 +30,11 @@ import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
 
-public class RoutinesFragment extends Fragment {
+public class RoutinesFragment extends Fragment implements AdapterView.OnItemSelectedListener{
 
     private RoutineListViewModel viewModel;
+
+    private TextView textViewFilter;
 
     private RoutinesAdapter routinesAdapter = new RoutinesAdapter(new ArrayList<>());
 
@@ -34,6 +43,8 @@ public class RoutinesFragment extends Fragment {
     private NestedScrollView nestedScrollView;
     private RecyclerView recylcerView;
     private ProgressBar progressBar;
+
+    private Spinner spinner;
 
     boolean noMoreEntries = false;
 
@@ -44,11 +55,25 @@ public class RoutinesFragment extends Fragment {
     public View onCreateView(@NotNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         binding = FragmentRoutinesBinding.inflate(getLayoutInflater());
 
+        View view = binding.getRoot();
+        textViewFilter = (TextView) view.findViewById(R.id.filter_textview);
+
+        textViewFilter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openFilterDialog();
+            }
+        });
+        spinner = view.findViewById(R.id.sortSpinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getActivity(),R.array.sort,android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(this);
+
         nestedScrollView = binding.scrollView;
         recylcerView = binding.recyclerView;
         progressBar = binding.progressBar;
 
-        View view = binding.getRoot();
 
         return view;
     }
@@ -72,5 +97,21 @@ public class RoutinesFragment extends Fragment {
                     }
                 });
     }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        String text = parent.getItemAtPosition(position).toString();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
+    }
+
+    public void openFilterDialog() {
+        FilterDialog filterDialog = new FilterDialog();
+        filterDialog.show(getParentFragmentManager(), "example dialog");
+    }
+
 
 }
