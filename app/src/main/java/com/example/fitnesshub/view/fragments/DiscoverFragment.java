7aa2e8b36c11e29362b735cc3d827d3db5afffc9
Ignacio Agustin Manfredi <imanfredi@itba.edu.ar>
2020-11-12 15:57:51 -0300

    add discoverFragment

--- app/src/main/java/com/example/fitnesshub/view/fragments/DiscoverFragment.java
+++ app/src/main/java/com/example/fitnesshub/view/fragments/DiscoverFragment.java
@@ -3,6 +3,8 @@ package com.example.fitnesshub.view.fragments;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -22,7 +24,6 @@ public class DiscoverFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,Bundle savedInstanceState) {
-        binding = FragmentDiscoverBinding.inflate(getLayoutInflater());
         return inflater.inflate(R.layout.fragment_discover, container, false);
     }
 }
