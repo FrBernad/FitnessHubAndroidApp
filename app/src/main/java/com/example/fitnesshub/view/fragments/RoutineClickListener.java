5db84457a4bc754b029749f7ab45a6ee7e2b4f7b
Francisco <frbernad@itba.edu.ar>
2020-11-12 11:18:08 -0300

    fixed fav routine click and added dynamic routine execution

---
+++ app/src/main/java/com/example/fitnesshub/view/fragments/RoutineClickListener.java
@@ -0,0 +1,42 @@
+package com.example.fitnesshub.model;
+
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.navigation.Navigation;
+
+import com.example.fitnesshub.R;
+import com.example.fitnesshub.view.fragments.ProfileFragmentDirections;
+import com.example.fitnesshub.view.fragments.RoutinesFragmentDirections;
+
+public class RoutineClickListener implements View.OnClickListener {
+
+    private RoutineData routineData;
+    private int from;
+    public static final int ROUTINES_ID = 1;
+    public static final int FAV_ID = 2;
+
+    public RoutineClickListener(RoutineData routineData, int from) {
+        this.routineData = routineData;
+        this.from = from;
+    }
+
+    @Override
+    public void onClick(View view) {
+        int routineId = Integer.parseInt(((TextView) view.findViewById(R.id.routineId)).getText().toString());
+        switch (from) {
+            case ROUTINES_ID:
+                RoutinesFragmentDirections.ActionRoutinesFragmentToRoutineFragment action1 = RoutinesFragmentDirections.actionRoutinesFragmentToRoutineFragment(routineData);
+                action1.setRoutineId(routineId);
+                action1.setRoutineData(routineData);
+                Navigation.findNavController(view).navigate(action1);
+                break;
+            case FAV_ID:
+                ProfileFragmentDirections.ActionMeFragmentToRoutineFragment action2 = ProfileFragmentDirections.actionMeFragmentToRoutineFragment(routineData);
+                action2.setRoutineId(routineId);
+                action2.setRoutineData(routineData);
+                Navigation.findNavController(view).navigate(action2);
+                break;
+        }
+    }
+}
