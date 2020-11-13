a2af49fc6ce66d477016fe253c3609113d723803
Francisco <frbernad@itba.edu.ar>
2020-11-12 23:42:59 -0300

    added permanent user information and token

---
+++ app/src/main/java/com/example/fitnesshub/model/AppPreferences.java
@@ -0,0 +1,38 @@
+package com.example.fitnesshub.model;
+
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+import com.example.fitnesshub.R;
+
+public class AppPreferences {
+    private final String AUTH_TOKEN = "auth_token";
+    private final String USER_ID = "user_id";
+
+    private SharedPreferences sharedPreferences;
+
+    public AppPreferences(Context context) {
+        sharedPreferences = context.getSharedPreferences(context.getString(R.string.app_name), Context.MODE_PRIVATE);
+    }
+
+    public void setAuthToken(String token) {
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString(AUTH_TOKEN, token);
+        editor.apply();
+    }
+
+    public String getAuthToken() {
+        return sharedPreferences.getString(AUTH_TOKEN, null);
+    }
+
+    public int getUserId() {
+        return sharedPreferences.getInt(USER_ID, -1);
+    }
+
+    public void setUserId(Integer id) {
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putInt(USER_ID, id);
+        editor.apply();
+    }
+}
