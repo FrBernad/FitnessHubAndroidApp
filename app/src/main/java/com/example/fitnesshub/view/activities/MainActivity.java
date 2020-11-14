fe9bf960d57c89a16a7b02ce1c8884c85076590e
Ignacio Agustin Manfredi <imanfredi@itba.edu.ar>
2020-11-14 20:34:02 -0300

    notifications

--- app/src/main/java/com/example/fitnesshub/view/activities/MainActivity.java
+++ app/src/main/java/com/example/fitnesshub/view/activities/MainActivity.java
@@ -2,28 +2,38 @@ package com.example.fitnesshub.view.activities;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.navigation.fragment.NavHostFragment;
 import androidx.navigation.ui.NavigationUI;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 
 import com.example.fitnesshub.R;
-import com.example.fitnesshub.databinding.ActivityMainBinding;
 import com.example.fitnesshub.viewModel.FavouritesRoutinesViewModel;
 import com.example.fitnesshub.viewModel.UserViewModel;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private BottomNavigationView bottomNavigationView;
 
+    private BottomNavigationView bottomNavigationView;
     private UserViewModel viewModel;
+
+    private static final String CHANNEL_ID = "simplified_coding";
+    private static final String CHANNEL_NAME = "Simplified_coding";
+    private static final String CHANNEL_DESC = "Simplified_coding notifications";
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,8 +44,34 @@ public class MainActivity extends AppCompatActivity {
         viewModel = new ViewModelProvider(this).get(UserViewModel.class);
         new ViewModelProvider(this).get(FavouritesRoutinesViewModel.class).updateData();
         viewModel.setUserData();
+
+        createNotificationChannel();
+
+        findViewById(R.id.notificationBtn).setOnClickListener(v -> displayNotification());
+        I
+
     }
 
+    public void createNotificationChannel(){
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, CHANNEL_NAME, NotificationManager.IMPORTANCE_DEFAULT);
+            channel.setDescription(CHANNEL_DESC);
+            NotificationManager manager = getSystemService(NotificationManager.class);
+            manager.createNotificationChannel(channel);
+        }
+    }
+
+    private void displayNotification(){
+        NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(this,CHANNEL_ID)
+                .setSmallIcon(R.drawable.logo)
+                .setContentTitle("Fitness Hub")
+                .setContentText("Rutina")
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+
+        NotificationManagerCompat mNotificationMgr = NotificationManagerCompat.from(this);
+        mNotificationMgr.notify(1,mBuilder.build());
+
+    }
 
     public void setUpBottomNavigation() {
         bottomNavigationView = findViewById(R.id.bottomNav);
